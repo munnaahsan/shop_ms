c09551060de360b8f9bb11bc853ed3ea2e16f436
Munna Ahsan <54517912+munnaahsan@users.noreply.github.com>
2021-01-11 17:45:01 +0600

    migration table create

---
+++ database/migrations/2021_01_11_114033_create_suppliers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSuppliersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->id();
+            $table->string('s_name');
+            $table->string('s_contact');
+            $table->string('s_email')->unique();
+            $table->string('s_address')->nullable();
+            $table->string('s_remarks')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('suppliers');
+    }
+}
