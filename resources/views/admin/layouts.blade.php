1784abed77a2ddd89bceb2dcb7588bda6618a389
Munna Ahsan <54517912+munnaahsan@users.noreply.github.com>
2021-01-13 17:55:44 +0600

    title change

--- resources/views/admin/layouts.blade.php
+++ resources/views/admin/layouts.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Blank Page</title>
+    <title>Shop MS</title>
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
