2d8c88d4e5ab97e17e846a30ef06662374889f94
Munna Ahsan <54517912+munnaahsan@users.noreply.github.com>
2021-01-24 11:43:59 +0600

    Product Table Correction

--- database/migrations/2021_01_11_112345_create_products_table.php
+++ database/migrations/2021_01_11_112345_create_products_table.php
@@ -15,13 +15,10 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('p_id');
             $table->string('p_name');
-            $table->string('p_cat_id');
-            // $table->number('cat_id'); // Foreign
-            $table->foreignId('cat_id');
+            $table->foreignId('category_id');
             $table->string('p_sku');
-            $table->number('p_uom_id');  //Foreign
+            $table->number('uom_id');  //Foreign
             $table->number('p_quantity');
             $table->number('p_price');
             $table->string('p_image');
