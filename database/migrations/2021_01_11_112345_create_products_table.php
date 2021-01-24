b7e5dde668b48596bc9d7adb4cb4099e1b534c9a
Munna Ahsan <54517912+munnaahsan@users.noreply.github.com>
2021-01-24 11:35:12 +0600

    product table

--- database/migrations/2021_01_11_112345_create_products_table.php
+++ database/migrations/2021_01_11_112345_create_products_table.php
@@ -15,13 +15,23 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('p_id');
             $table->string('p_name');
-            $table->string('p_price');
+            $table->string('p_cat_id');
+            // $table->number('cat_id'); // Foreign
+            $table->foreignId('cat_id');
+            $table->string('p_sku');
+            $table->number('p_uom_id');  //Foreign
+            $table->number('p_quantity');
+            $table->number('p_price');
+            $table->string('p_image');
+            $table->string('p_color')->nullable();
+            $table->string('p_size');           
             $table->string('p_brand');
-            $table->string('p_quantity');
-            $table->string('p_barcode');
-            $table->string('p_status');
+            $table->string('p_barcode')->unique();
+            $table->boolean('p_status');            
+            $table->string('p_keyword');
+            $table->string('p_remarks');
             $table->foreign('category_id')->references('id')->on('categories')->cascadeOnDelete();
             $table->timestamps();
 
